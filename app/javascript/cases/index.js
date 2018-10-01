19e691ab5ac023b8622f08a29574ea4f39758390
Harold <harold.benard@hotmail.fr>
2018-10-01 07:11:50 +0000

    starting editing table for cases

--- app/javascript/cases/index.js
+++ app/javascript/cases/index.js
@@ -1,11 +1,114 @@
-import Vue from "vue/dist/vue.common";
 
 console.log("hello");
 
-new Vue({
+const vue = new Vue({
     el: '#vue',
     data: {
+        editModal: false,
         cases: gon.cases,
-        
+        columns: [
+            {
+                title: "Etat",
+                key: "state",
+                align: 'center',
+                width: 100
+            },
+            {
+                title: "Order",
+                key: "order",
+                align: 'center',
+                width: 90
+            },
+            {
+                title: "Ticket",
+                key: "ticket",
+                align: 'center',
+                width: 80
+            },
+            {
+                title: "Client",
+                key: "client",
+                align: 'center'
+            },
+            {
+                title: "Adresse",
+                key: "address",
+                align: 'center'
+            },
+            {
+                title: "Suivi",
+                key: "tracking",
+                align: 'center'
+            },
+            {
+                title: "Sujet",
+                key: "subject",
+                align: 'center'
+            },
+            {
+                title: "Notes",
+                key: "note",
+                align: 'center'
+            },
+            {
+                title: "Enquête",
+                key: "inquiry",
+                align: 'center',
+                width: 110
+            },
+            {
+                title: "Action",
+                key: "action",
+                align: 'center'
+            },
+            {
+                title: 'Action',
+                key: 'action',
+                width: 150,
+                align: 'center',
+                render: (h, params) => {
+                    return h('div', [
+                        h('Button', {
+                            props: {
+                                type: 'primary',
+                                size: 'small'
+                            },
+                            style: {
+                                marginRight: '5px'
+                            },
+                            on: {
+                                click: () => {
+                                    console.log(this)
+                                    vue.show(params.index);
+                                }
+                            }
+                        }, 'View'),
+                        h('Button', {
+                            props: {
+                                type: 'error',
+                                size: 'small'
+                            },
+                            on: {
+                                click: () => {
+                                    this.remove(params.index);
+                                }
+                            }
+                        }, 'Delete')
+                    ]);
+                }
+            },
+        ]
+    },
+    methods: {
+        show(index) {
+            console.log(index);
+            this.editModal = true;
+        },
+        updateCase() {
+            console.log('UPDATE');
+        }
     }
 });
+
+
+window.vue = vue;
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,5 @@
 
 user = User.find_by(email:"harold@lesraffineurs.com")
 (0..10).each do |i|
-    Case.create(user_id: user.id, state: "Nouveau #{i}", order: "23456 #{i}", client: "Dominique Bénard #{i}", address: "31 rue de Toulouse #{i}", category: "problème livraison #{i}", subject: "livré mais non reçu #{i}", note: "client contacté, en attente réponse #{i}", ticket: "27645 #{i}")
+    Case.create(user_id: user.id, state: "Nouveau #{i}", order: "23456 #{i}", client: "Dominique Bénard #{i}", address: "31 rue de Toulouse #{i}", category: "problème livraison #{i}", subject: "livré mais non reçu #{i}", note: "client contacté, en attente réponse #{i}", ticket: "27645 #{i}", inquiry: "23 456 23 #{i}", tracking: "2L112414141312312")
 end
